d81e206fdd6c18b7c44e44d50973c510722df572
Washington <washington.contato1327@gmail.com>
2024-08-09 16:22:39 -0300

    Adicionando uma pasta data ao projeto para armazenar valores das seções

---
+++ src/data/session-data.js
@@ -0,0 +1,60 @@
+const sessionData = {
+  session1: {
+    title: "Formação acadêmica",
+    paragraphs: [
+      "Análise e Desenvolvimento de Sistemas - Concluído",
+      "Engenharia de Software - Cursando",
+    ],
+    certificatesButton: "Certificados",
+    resumeButton: "Currículo",
+    portfolioLink: "Portfólio",
+  },
+  session2: {
+    title: "Sobre mim",
+    paragraphs: [
+      "Olá, sou Washington Lemos. Sou apaixonado por tecnologia e desenvolvimento de software, com uma forte dedicação em evoluir e crescer na área de programação. Atualmente, estou cursando Engenharia de Software e trabalhando como desenvolvedor front-end, com experiência em projetos pessoais e conhecimento em tecnologias como HTML, CSS, JavaScript, React e Next.js. Meu objetivo de carreira é consolidar minha posição na área de tecnologia e contribuir para projetos inovadores que façam a diferença. Estou sempre em busca de novos desafios e oportunidades para aprender e me aprimorar.",
+      "Objetivos de Carreira: Minha meta é me destacar na área de desenvolvimento front-end, contribuindo para projetos de impacto e alcançando reconhecimento profissional. Busco oportunidades que me permitam aplicar meus conhecimentos, aprender novas tecnologias e colaborar com equipes criativas e inovadoras.",
+      "Áreas de Interesse na Programação: Tenho interesse em desenvolvimento front-end e estou expandindo meus conhecimentos em engenharia de software, desenvolvimento full stack e banco de dados. Acredito que entender tanto o front-end quanto o back-end é crucial para criar aplicações completas e funcionais. Estou sempre em busca de novos desafios para aplicar e aprimorar essas habilidades.",
+    ],
+  },
+  session3: {
+    title: "Gostos Pessoais",
+    paragraphs: [
+      "Sou uma pessoa apaixonada por viajar e explorar novos lugares. Entre os diversos destinos que já tive a oportunidade de conhecer pelo Brasil, a Chapada dos Veadeiros se destacou como o mais marcante. Lá, tive a chance de me conectar profundamente com a natureza através de suas cachoeiras deslumbrantes e trilhas revitalizantes. A natureza exerce um efeito renovador em mim, e sempre busco estar em contato com ela.",
+      "Além das viagens, tenho um grande apreço por esportes como futebol e ciclismo, que me ajudam a manter um estilo de vida ativo e saudável. No universo dos games, sou fã do estilo battlegrounds, que sempre me proporciona momentos de diversão e desafio. Valorizo muito o tempo com minha família e adoro aproveitar os fins de semana para estar com eles, seja em atividades ao ar livre ou em momentos de lazer.",
+      "Sou um grande apreciador de filmes e séries, e meus gêneros preferidos são os de guerra e de heróis. As histórias intensas e inspiradoras desses gêneros sempre me envolvem e me fascinam.",
+    ],
+  },
+  session4: {
+    title: "Motivações",
+    paragraphs: [
+      "Minha principal motivação para participar da comunidade Código Certo Coders é a oportunidade de crescer profissionalmente e me conectar com outros desenvolvedores apaixonados por tecnologia. Acredito que a comunidade oferece um ambiente enriquecedor para compartilhar conhecimentos, aprender novas habilidades e colaborar em projetos desafiadores. Quero me envolver ativamente, contribuir com minhas habilidades e, ao mesmo tempo, absorver experiências e insights de outros membros. Estou determinado a utilizar essa plataforma para acelerar minha evolução na área de desenvolvimento front-end, alcançar meus objetivos de carreira e mostrar meu potencial em um espaço que valoriza a inovação e o aprendizado contínuo.",
+    ],
+  },
+  session5: {
+    title: "Confira meus últimos projetos",
+    paragraphs: [
+      "Confira meus últimos projetos em desenvolvimento. Você pode visualizar e acompanhar o progresso dos projetos que estou trabalhando atualmente.",
+    ],
+    cards: [
+      // Exemplo de dados dos cards, ajuste conforme necessário
+      {
+        title: "Projeto 1",
+        description: "Descrição do Projeto 1",
+        link: "#",
+      },
+      {
+        title: "Projeto 2",
+        description: "Descrição do Projeto 2",
+        link: "#",
+      },
+      {
+        title: "Projeto 3",
+        description: "Descrição do Projeto 3",
+        link: "#",
+      },
+    ],
+  },
+};
+
+export default sessionData;
