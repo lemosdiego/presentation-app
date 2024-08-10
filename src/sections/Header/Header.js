15207e9c1d03913b7deec96a74c33ee13a63201c
Washington <washington.contato1327@gmail.com>
2024-08-10 08:54:13 -0300

    Criando menu navegação

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -1,16 +1,47 @@
+"use client";
 import StylesHeader from "./StylesHeader.module.css";
 
 const Header = () => {
+  function abrirMenu() {
+    const header = document.querySelector("#header");
+    const fechar = document.querySelector("#fechar");
+    const abrir = document.querySelector("#abrir");
+
+    header.style.display = "flex";
+    fechar.style.display = "block";
+    abrir.style.display = "none";
+  }
+  function fecharMenu() {
+    const header = document.querySelector("#header");
+    const fechar = document.querySelector("#fechar");
+    const abrir = document.querySelector("#abrir");
+
+    header.style.display = "none";
+    fechar.style.display = "none";
+    abrir.style.display = "block";
+  }
   return (
     <>
       <div className={StylesHeader.container_menu}>
         <div className={StylesHeader.icon}>
-          <img />
+          <img src="/img/codigocerto.png" alt="Icone codigo certo" />
         </div>
-        <p>Menu</p>
+        <p className={StylesHeader.abrir} id="abrir" onClick={abrirMenu}>
+          Menu
+        </p>
+        <p className={StylesHeader.fechar} id="fechar" onClick={fecharMenu}>
+          Fechar
+        </p>
       </div>
-      <header className={StylesHeader.header}>
-        <nav></nav>
+      <header className={StylesHeader.header} id="header">
+        <nav className={StylesHeader.nav}>
+          <ul>
+            <li>Inicio</li>
+            <li>Sobre</li>
+            <li>Gostos</li>
+            <li>Motivações</li>
+          </ul>
+        </nav>
       </header>
     </>
   );
